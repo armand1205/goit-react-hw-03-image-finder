e73ac019468b50d97d11042cdeacb0af0af0c8ab
armand1205 <munteanuarmandshadow1205@gmail.com>
2025-02-26 16:41:33 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Loader from './Loader/Loader';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Loader />
     </div>
   );
 };
