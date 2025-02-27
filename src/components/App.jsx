e0b734581a96db54686e32efa33aaf5d385e5d1c
armand1205 <135024222+armand1205@users.noreply.github.com>
2025-02-27 16:41:12 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Loader from './Loader/Loader';
 
 export const App = () => {
   return (
@@ -12,8 +11,6 @@ export const App = () => {
         fontSize: 40,
         color: '#010101',
       }}
-    >
-      <Loader />
-    </div>
+    ></div>
   );
 };
